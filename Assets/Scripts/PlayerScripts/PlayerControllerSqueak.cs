946b2f98cd74191c1a8d70479adf4e4b2dd2fc48
chloeylee <chloester11204@gmail.com>
2024-11-01 04:38:36 -0400

    all anims added, anim controller for squeak done
    FIXME: weird offset in the idle anim and walking anim. also walking anim is offset that puts it 'below ground'

--- Assets/Scripts/PlayerScripts/PlayerControllerSqueak.cs
+++ Assets/Scripts/PlayerScripts/PlayerControllerSqueak.cs
@@ -23,38 +23,57 @@ public class PlayerControllerSqueak : MonoBehaviour
     private KeyCode jumpKey = KeyCode.UpArrow;       // Jump key
     private KeyCode crouchKey = KeyCode.DownArrow;     // Crouch key
 
+    private Animator animator;
+
     // Start is called before the first frame update
     void Start()
     {
         crouchFlag = false;
         standingCollider.enabled = true;
         crouchingCollider.enabled = false;
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        // TODO: split this up into helpers
         horizontal = Input.GetAxisRaw("Horizontal");
 
         if (Input.GetKey(moveLeftKey))
         {
             horizontal = -1f; // move left
+            animator.SetBool("isWalking", true);
         }
         if (Input.GetKey(moveRightKey))
         {
             horizontal = 1f; // move right
+            animator.SetBool("isWalking", true);
+        }
+        if (!Input.GetKey(moveLeftKey) && !Input.GetKey(moveRightKey))
+        {
+            horizontal = 0f; // stop moving
+            animator.SetBool("isWalking", false);
         }
 
         // Jumping logic
         if (Input.GetKey(jumpKey) && IsGrounded())
         {
+            animator.SetBool("isJumping", true);
             rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+
+        }
+        else
+        {
+            animator.SetBool("isJumping", false);
         }
 
         // Crouching logic
+        // TODO: change anim if crouching vs crouch walking
         if (Input.GetKeyDown(crouchKey) && !crouchFlag) // Start crouching
         {
             crouchFlag = true;
+            animator.SetBool("isCrouching", true);
         }
         else if (Input.GetKeyUp(crouchKey) && crouchFlag) // Stop crouching
         {
@@ -62,6 +81,7 @@ public class PlayerControllerSqueak : MonoBehaviour
             if (!Physics2D.OverlapCircle(overheadCheckCollider.position, overheadCheckRadius, groundLayer))
             {
                 crouchFlag = false;
+                animator.SetBool("isCrouching", false);
             }
         }
 
